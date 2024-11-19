65accb44ac3c5b34a08cc40fc7c8b9933db86625
ttheconjuring <90863683+ttheconjuring@users.noreply.github.com>
2024-11-19 16:46:30 +0200

    initial commit

---
+++ src/main/java/pesho/bg/oath2demoo/Oath2DemooApplication.java
@@ -0,0 +1,13 @@
+package pesho.bg.oath2demoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oath2DemooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oath2DemooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pesho/bg/oath2demoo/Oath2DemooApplicationTests.java
@@ -0,0 +1,13 @@
+package pesho.bg.oath2demoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oath2DemooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
